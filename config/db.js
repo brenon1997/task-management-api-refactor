5bd1e4bb6f331795aa52718c5bf2efde47603b53
Brenon Tavares <brenonperez1997@gmail.com>
2023-10-04 23:28:25 -0300

    chore: init project

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const { mongodb_url } = require('../.env')
+
+console.log(mongodb_url)
+mongoose.connect(mongodb_url, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}).then(() => {
+    console.log('Conectado ao MongoDB')
+}).catch(error => {
+    const msg = 'Erro ao conectar ao MongoDB'
+    console.log(msg)
+})
